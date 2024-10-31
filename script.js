e8748bfbc5a97a664d9306f90962ad68c9c55021
Nindy <macbookpro@nindyeriyani.local>
2024-11-01 01:05:38 +0800

    add modal

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to open the modal
+function openModal() {
+  document.getElementById("imageModal").style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
+}
