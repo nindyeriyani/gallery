5abd3516b63a89d40a85486ea450f5ae712c6ce0
Nindy <macbookpro@nindyeriyani.local>
2024-11-08 00:51:50 +0800

    add clean code

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-// Function to open the modal
+const galleryData = [
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+  { src: 'img/cover.png', title: 'Tittle Right Here', description: 'Description about your stuff will be shown right here with Text Align center so it will be so beauty~!' },
+];
+
+const galleryContainer = document.querySelector('.photos');
+
+galleryData.forEach(item => {
+  const photoDiv = document.createElement('div');
+  photoDiv.classList.add('photos');
+  
+  photoDiv.innerHTML = `
+    <img src="${item.src}" alt="${item.title}">
+    <div class="photo-infos">
+      <h3 class="title">${item.title}</h3>
+      <p class="desc">${item.description}</p>
+    </div>
+  `;
+  
+  galleryContainer.appendChild(photoDiv);
+});
+
+document.querySelectorAll('.photos').forEach(item => {
+  item.addEventListener('click', openModal);
+});
+
 function openModal() {
-  document.getElementById("imageModal").style.display = "block";
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "block";
 }
 
-// Function to close the modal
 function closeModal() {
-  document.getElementById("imageModal").style.display = "none";
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "none";
 }
+
+document.querySelector(".close").addEventListener("click", closeModal);
